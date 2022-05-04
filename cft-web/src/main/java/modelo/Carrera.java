0920e4d2c410d307449ed3476578f70f96a6ce66
Christian Coloma <100450987+CColomaC@users.noreply.github.com>
2022-05-04 13:50:07 -0400

    Update Carrera.java

--- cft-web/src/main/java/modelo/Carrera.java
+++ cft-web/src/main/java/modelo/Carrera.java
@@ -1,10 +1,34 @@
 package modelo;
 
 public class Carrera {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// ATRIBUTOS O PROPIEDADES O VARIABLES DE INSTANCIA 
+	private int id;
+	private String nombre;
+	
+	// CONSTRUCTORES 	
+	public Carrera() {
+	}
+	
+	public Carrera(String nombre) {
+		this.nombre = nombre;
 	}
 
+	public Carrera(int id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	// GETTERS Y SETTERS 
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}		
 }
